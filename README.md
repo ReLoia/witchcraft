2f594818c01380298080f94b6aafd108fafe3fe4
ReLoia <zrennyz@gmail.com>
2024-06-21 00:01:51 +0200

    First Commit Started making the home

---
+++ README.md
@@ -0,0 +1,26 @@
+# WitchSand
+
+WitchSand is a website made as a HackClub project.  
+It is a website that allows users to make and share sandwiches made with any ingredient.
+
+### How is it made?
+WitchSand is made with HTML, CSS, JavaScript, Python, and SQLite. 
+The frontend is made with Vue.js.  
+The backend is made with FastAPI and SQLite.
+
+## ToDo
+- [ ] Create the frontend
+- [ ] Create the backend
+- [ ] Create the database
+- [ ] Create the API
+- [ ] Allow users to create sandwiches
+- [ ] Allow users to view sandwiches
+- [ ] Allow users to delete sandwiches
+- [ ] Allow users to edit sandwiches
+- [ ] Allow users to search for sandwiches
+- [ ] Allow users to view the ingredients of a sandwich  
+\\> with an animation when pressing on a sandwich, all ingredients will be shown side by side  
+\\> on mobile, one under the other  
+- [ ] Allow users to view the author of a sandwich
+- [ ] Be able to show all the sandwiches you made and liked as a widget for external websites 
+- [ ] Deploy the website
---
+++ frontend/src/App.vue
@@ -0,0 +1,64 @@
+<script setup>
+</script>
+
+<template>
+  <div class="title">
+    <h2>witch</h2>
+    <h2>CRAFT</h2>
+    <div id="underline">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        h2Color: '#fff'
+      }
+    },
+    methods: {
+      randomColor() {
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+          color += Math.floor(Math.random() * 16).toString(16);
+        }
+        return color;
+      }
+    },
+  };
+
+  setInterval(() => {
+    this.h2Color = this.randomColor();
+  }, 1000);
+</script>
+
+<style scoped>
+  .title {
+    text-align: center;
+    font-size: 4rem;
+    line-height: .75;
+    width: fit-content;
+    transform: rotateZ(350deg);
+
+    & > h2 {
+      margin: 0;
+      padding: 0;
+
+      &:nth-child(2) {
+        color: v-bind(h2Color);
+      }
+    }
+
+    & > div#underline {
+      width: 100%;
+      height: 14px;
+      background-color: black;
+      margin: 10px auto;
+      border-radius: 24px;
+
+    }
+  }
+</style>
