a3bbf73b3bf8ce3507c95121d56a11c2c5109b52
ReLoia <zrennyz@gmail.com>
2024-06-21 16:05:40 +0200

    Started writing App.vue Made a Design in Figma Small fixes to css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,64 +1,109 @@
 <script setup>
+
 </script>
 
 <template>
-  <div class="title">
-    <h2>witch</h2>
-    <h2>CRAFT</h2>
+  <header>
+    <div class="title">
+    <span>witch</span>
+    <span>CRAFT</span>
     <div id="underline">
 
     </div>
-  </div>
+    </div>
+    <div class="stats">
+      <div class="digits">
+        <span>0</span>
+        <span>0</span>
+        <span>0</span>
+        <span>0</span>
+      </div>
+      <p>
+        <span>sandwitches</span> <span>crafted</span>
+      </p>
+    </div>
+  </header>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    data() {
-      return {
-        h2Color: '#fff'
-      }
-    },
-    methods: {
-      randomColor() {
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-          color += Math.floor(Math.random() * 16).toString(16);
-        }
-        return color;
-      }
-    },
-  };
 
-  setInterval(() => {
-    this.h2Color = this.randomColor();
-  }, 1000);
 </script>
 
 <style scoped>
-  .title {
-    text-align: center;
-    font-size: 4rem;
-    line-height: .75;
-    width: fit-content;
-    transform: rotateZ(350deg);
+  header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
 
-    & > h2 {
-      margin: 0;
-      padding: 0;
+    padding-top: 20px;
+    height: 90px;
 
-      &:nth-child(2) {
-        color: v-bind(h2Color);
+    & > .title {
+      text-align: center;
+      font-size: 3.6rem;
+      line-height: .75;
+      width: fit-content;
+
+      transform: scale(.6) rotate(-3deg);
+
+      display: flex;
+      flex-direction: column;
+      letter-spacing: -4px;
+
+      & > span {
+        margin: 0;
+        padding: 0;
+        font-size: .9em;
+
+
+        &:nth-child(2) {
+          font-size: 1.1em;
+          font-weight: bold;
+          color: var(--primary);
+        }
+      }
+
+      & > div#underline {
+        width: 110%;
+        height: 8px;
+
+        transform: translate(-4%, 0);
+
+        background-color: #D9D9D9;
+        margin: 10px auto;
+        border-radius: 24px;
       }
     }
 
-    & > div#underline {
-      width: 100%;
-      height: 14px;
-      background-color: black;
-      margin: 10px auto;
-      border-radius: 24px;
+    & > .stats {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
 
+      transform: rotate(3deg);
+
+      & > .digits {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+
+        & > span {
+          font-size: 3rem;
+          line-height: .75;
+          font-weight: bold;
+        }
+      }
+
+      & > p {
+        font-size: .85em;
+        color: #D9D9D9;
+      }
     }
   }
+
 </style>
